6e003e9eea555d16348905a23ed766b5947dfb61
Michael Herrmann <michael@herrmann.io>
2020-03-22 12:49:32 +0100

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # i3lock-matrix
-Show the Matrix as i3's lock screen
+
+![i3lock-matrix Demo](demo.gif)
+
+The script [matrixlock.py](matrixlock.py) in this repository lets you use the
+Matrix as i3's lock screen. It uses Python 3, _xfce4-terminal_, _cmatrix_ and
+_bash_. You should not find it too difficult to edit the script to use a
+different stack.
+
+## Requirements
+
+ * Python 3.7+
+ * Bash
+ * Compton
+ * xfce4-terminal
+ * [cmatrix](https://github.com/abishekvashok/cmatrix)
+
+You should not find it too difficult to edit [matrixlock.py](matrixlock.py) to
+use a slightly different stack, such as a different terminal. If you do do that,
+feel free to submit a Pull Request to share your work with others.
+
+## Set up
+
+Make sure you have all of the above requirements.
+
+Add the following to your Compton config:
+
+    opacity-rule = ["10:name = 'i3lock'"]
+
+(If you want more / less transparency of the circle while typing, change `10`).
+
+I do this by having the above line in a file `compton.conf` and in my i3 config:
+
+    exec --no-startup-id compton --config ~/.config/i3/compton.conf
+
+Download [matrixlock.py](matrixlock.py) and make it executable:
+
+    chmod +x matrixlock.py
+
+Then, you should already be able to do:
+
+    ./matrixlock.py
+
+To automatically lock the screen after 5 minutes, add the following to your
+i3 config:
+
+    exec --no-startup-id xautolock -time 5 -locker ~/.config/i3/matrixlock.py
+
+(Install `xautolock` if you don't have it.)
+
+I furthermore use the following power menu:
+
+```
+set $Locker ~/.config/i3/matrixlock.py 1 & sleep 1
+
+set $mode_system System (l) lock, (e) logout, (s) suspend, (h) hibernate, (r) reboot, (Shift+s) shutdown
+mode "$mode_system" {
+    bindsym l exec --no-startup-id $Locker, mode "default"
+    bindsym e exec --no-startup-id i3-msg exit, mode "default"
+    bindsym s exec --no-startup-id $Locker && systemctl suspend, mode "default"
+    bindsym h exec --no-startup-id $Locker && systemctl hibernate, mode "default"
+    bindsym r exec --no-startup-id systemctl reboot, mode "default"
+    bindsym Shift+s exec --no-startup-id systemctl poweroff -i, mode "default"  
+
+    # back to normal: Enter or Escape
+    bindsym Return mode "default"
+    bindsym Escape mode "default"
+}
+
+bindsym $mod+Pause mode "$mode_system"
+bindsym --release XF86PowerOff mode "$mode_system"
+```
+
+Note `matrixlock` in the first line and that it only uses a single ampersand.
+The last two lines let me invoke the power menu when I press
+<kbd>Mod</kbd>+<kbd>Pause</kbd> or the Power button on my laptop.
+
+## Acknowledgements
+
+This script would not have been possible without
+[this Youtube video](https://www.youtube.com/watch?v=jDyQHt2Iiro).
---
+++ matrixlock.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+from argparse import ArgumentParser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os.path import dirname, join
+from subprocess import run, Popen, DEVNULL
+from threading import Thread, Event
+from time import time
+
+import json
+
+def main(matrix_delay_secs):
+	workspaces = get_workspaces()
+	visible = [ws for ws in workspaces if ws['visible']]
+	with SubprocessServer(('', 0), len(visible)) as server:
+		port = server.server_address[1]
+		for ws in visible:
+			overlay_matrix_on_workspace(ws['num'], port, matrix_delay_secs)
+	run(['i3lock', '-n'], check=True)
+	for pid_path in server.received_posts:
+		assert pid_path.startswith('/'), pid_path
+		try:
+			pid = int(pid_path[1:])
+		except ValueError:
+			continue
+		run(['kill', str(pid)])
+
+def get_workspaces():
+	cp = run(
+		['i3-msg', '-t', 'get_workspaces'],
+		capture_output=True, check=True, text=True
+	)
+	return json.loads(cp.stdout)
+
+def overlay_matrix_on_workspace(ws_num, port, delay):
+	run([
+		'i3-msg',
+		f'workspace {ws_num}; '
+		f'exec "xfce4-terminal --hide-scrollbar --hide-menubar --fullscreen --color-text=black '
+		# Send child PID to server so the parent can kill it, then show Matrix:
+		f'-x bash -c \'curl -X POST localhost:{port}/$$ && sleep {delay} && cmatrix -b\'"'
+	], check=True, stdout=DEVNULL)
+
+class SubprocessServer(HTTPServer):
+	"""
+	Process up to num_requests POST requests in up to timeout_secs seconds and
+	store their paths in self.received_posts.
+	"""
+	def __init__(self, server_address, num_requests, timeout_secs=5):
+		super().__init__(server_address, SubprocessHandler)
+		self.received_posts = []
+		self._num_requests = num_requests
+		self._timeout_secs = timeout_secs
+		self._thread = Thread(target=self._run_in_thread)
+		self._started = Event()
+		self._timeout_encountered = False
+	def __enter__(self):
+		result = super().__enter__()
+		self._thread.start()
+		self._started.wait()
+		return result
+	def __exit__(self, *args, **kwargs):
+		self._thread.join()
+	def _run_in_thread(self):
+		self._started.set()
+		end = time() + self._timeout_secs
+		for _ in range(self._num_requests):
+			time_remaining = end - time()
+			if time_remaining < 0:
+				break
+			self.timeout = time_remaining
+			self.handle_request()
+			if self._timeout_encountered:
+				break
+	def handle_timeout(self):
+		self._timeout_encountered = True
+
+class SubprocessHandler(BaseHTTPRequestHandler):
+	def do_POST(self):
+		self.server.received_posts.append(self.path)
+		self.send_response(200)
+		self.end_headers()
+	def log_message(self, format, *args):
+		return
+
+if __name__ == '__main__':
+	parser = ArgumentParser(description='Alternative to i3lock that displays the Matrix')
+	parser.add_argument(
+		'delay', type=int, nargs='?', default=0,
+		help='Seconds between blanking out the screen and starting the Matrix'
+	)
+	args = parser.parse_args()
+	main(args.delay)
